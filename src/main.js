7d3d7fe9e2521a2681ae0f50a3f94eb3047404f2
pazcalll <yazeedarifin@gmail.com>
2022-10-28 20:46:38 +0700

    quasar init

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+// import './assets/main.css'
+import { Quasar } from 'quasar'
+import quasarUserOptions from './quasar-user-options'
+import "quasar/dist/quasar.sass"
+
+createApp(App).use(Quasar, quasarUserOptions).mount('#app')
